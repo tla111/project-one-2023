85bffed3dceec3b2959335543e2e2ab44ad4a7c8
Tim La <tlamarketing111@gmail.com>
2023-02-16 09:18:26 -0500

    created day component

---
+++ client/src/components/Day.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Day = () => {
+    return (
+        <div>
+            <ul>
+                <ol>02/14/23</ol>
+                <ol>02/15/23</ol>
+                <ol>02/16/23</ol>
+            </ul>
+        </div>
+    )
+}
+
+export default Day
