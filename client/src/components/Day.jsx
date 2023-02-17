2e6809317523653e6d8c69d1d525c4be6c396840
Tim La <tlamarketing111@gmail.com>
2023-02-17 13:42:30 -0500

    displayed row

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Day = () => {
     return (
-        <div>
+        <div style={{ display: "flex" }}>
             <ul>
                 <ol>02/14/23</ol>
                 <ol>02/15/23</ol>
                 <ol>02/16/23</ol>
+                <ol>02/17/23</ol>
+            </ul>
+            <ul>
+                <ol>Inactivity</ol>
+                <ol>Inactivity</ol>
+                <ol>Inactivity</ol>
+                <ol>Inactivity</ol>
             </ul>
         </div>
     )
