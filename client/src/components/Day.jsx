4ec23a2fe62ed1790464e727af5d1bb07bbe94ed
Tim La <tlamarketing111@gmail.com>
2023-03-15 08:51:51 -0400

    day 8

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -29,8 +29,10 @@ const Day = () => {
                 <ol>03/12/23</ol>
                 <ol>03/13/23</ol>
                 <ol>03/14/23</ol>
+                <ol>03/15/23</ol>
             </ul>
             <ul>
+                <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
