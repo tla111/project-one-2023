6b97fb13cf2b5e85452476de650eab4a9f54f6a1
Tim La <tlamarketing111@gmail.com>
2023-04-25 06:46:37 -0400

    day one

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -66,8 +66,10 @@ const Day = () => {
                 <ol>04/21/23</ol>
                 <ol>04/22/23</ol>
                 <ol>04/23/23</ol>
+                <ol>04/25/23</ol>
             </ul>
             <ul>
+                <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
                 <ol>Inactivity</ol>
